4d626b943e7d532e8b6ac37f472a753f171cba85
Xingyu Wang <brandon.xyw@gmail.com>
2019-09-16 17:20:32 -0400

    response

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var tracking = require('../packageInfo/TrackApi.js');
+
+
+var usps_username = '328NOCOM1209';     // DO NOT CHANGE THIS
+var tracking_number = '9500115483499149486703';
+
+function usps_callback(response) {
+  res.send(JSON.stringify(response));
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  tracking.trackUSPS(usps_username, tracking_number, usps_callback)
+  //res.render('index', { title: 'Ex' });
 });
 
 module.exports = router;
