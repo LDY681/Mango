d45edf9a5505315e850e715e26e9306b8a324bf7
Xingyu Wang <brandon.xyw@gmail.com>
2019-09-18 15:21:24 -0400

    test index page, tracking doesn't work

--- project/packageInfo/test.js
+++ project/packageInfo/test.js
@@ -1,16 +1,17 @@
 var tracking = require('./TrackApi.js');
 
+  var trackingID = '00000';
 
   // NO ERROR CHECK!
   // response is a JSON object
   function usps_callback(response) {
 
     // stringfy is as same as toString()
-    console.log(JSON.stringify(response));
+    //console.log(JSON.stringify(response));
 
     // print tracking number
     console.log(response.TrackResponse.TrackInfo[0].$);
-
+    trackingID = response.TrackResponse.TrackInfo[0].$
 
     // print summary
     console.log(response.TrackResponse.TrackInfo[0].TrackSummary);
@@ -29,10 +30,10 @@ var tracking = require('./TrackApi.js');
 //tracking.trackUSPS(usps_username, tracking_number, usps_callback);
 
 
-function ups_callback(response) {
-  // print toString()
-  console.log(JSON.stringify(response));
-}
+// function ups_callback(response) {
+//   // print toString()
+//   console.log(JSON.stringify(response));
+// }
 
 
 var ups_tracking_number = '1Z9YF1281326222692';
@@ -42,6 +43,18 @@ var accessKey = 'CD6B1F9260161CF5';
 var password = 'fuckUSPS!';
 var ups_username = 'wang3695';
 
-tracking.trackUPS(ups_username, password, accessKey, ups_tracking_number, ups_callback);
+//tracking.trackUPS(ups_username, password, accessKey, ups_tracking_number, ups_callback);
 
 
+
+function showInfo() {
+  console.log('works');
+  tracking.trackUSPS(usps_username, tracking_number, usps_callback);
+  //console.log(trackingID)
+  //while (trackingID == '00000');
+  console.log(trackingID)
+  return trackingID
+}
+
+
+exports.showInfo = showInfo
