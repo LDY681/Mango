a5d504d51284483edb723b20ebdf3454f0fea202
WUu422 <wu1037@purdue.edu>
2019-10-04 13:08:54 -0400

    more verification for tracking id

--- Mango/routes/veri.js
+++ Mango/routes/veri.js
@@ -5,7 +5,6 @@ router.post('/', function(req, res, next) {
     //var usps_username = req.body.usps_username;
     var tracking_number = req.body.tracking_number;
     var err_message = "";
-    //console.log('err_message: ', req.err_message);
 
     var id_vali = 1;
     if(tracking_number.length < 20){
@@ -16,22 +15,41 @@ router.post('/', function(req, res, next) {
         id_vali = -1;
         err_message = "(invalid) tracking number length > 25";
     }
-    else if(tracking_number.match(/[a-z]/)){
-        id_vali = -1;
-        err_message = "(invalid) tracking number has lowercase";
+    else if (tracking_number.match(/[a-z]/)){
+        if(tracking_number.match(/^[a-z]+$/)){
+            id_vali = -1;
+            err_message = "(invalid) tracking number only has lowercase letters";
+        }
+        else{
+            id_vali = -1;
+            err_message = "(invalid) tracking number has lowercase letters";
+        }
     }
     else if(tracking_number.match(/[A-Z]/)){
-        id_vali = -1;
-        err_message = "(invalid) tracking number has uppercase";
+        if(tracking_number.match(/^[A-Z]+$/)){
+            id_vali = -1;
+            err_message = "(invalid) tracking number only has uppercase letters";
+        }
+        else{
+            id_vali = -1;
+            err_message = "(invalid) tracking number has uppercase letters";
+        }
     }
-    else if(tracking_number.match(/[?=.*[!@#$%^&*?\(\)]]/)){
-        id_vali = -1;
-        err_message = "(invalid) tracking number has special characters";
+    else if(tracking_number.match(/[?=.*?[#?!@$%^&*-]/)){
+        if(tracking_number.match(/^[?=.*?[#?!@$%^&*-]+$/)){
+            id_vali = -1;
+            err_message = "(invalid) tracking number only has uppercase letters";
+        }
+        else{
+            id_vali = -1;
+            err_message = "(invalid) tracking number has special characters";
+        }
     }
-    else if (!tracking_number.match(/[0-9]/)) {
+    else if (!tracking_number.match(/^[0-9]+$/)) {
         id_vali = -1;
         err_message = "(invalid) tracking number does not have numbers";
     }
+
 
     if (id_vali === -1){
         //tracking_number is invalid
