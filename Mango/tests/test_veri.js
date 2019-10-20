d6e43c1ab0a86b773e3bf6796a5750c945ddf622
WUu422 <wu1037@purdue.edu>
2019-10-20 13:50:28 -0400

    update verification and test of tracking id

--- Mango/tests/test_veri.js
+++ Mango/tests/test_veri.js
@@ -2,11 +2,11 @@ var request = require("supertest");
 var server = require("../app.js");
 var assert = require("assert");
 
-it('Test 1: invalid tracking number when length < 20', function(done)
+it('Test 1: invalid tracking number when length = 19 < 20 (Boundary)', function(done)
 {
     let data = {
         usps_username: '328NOCOM1209',
-        tracking_number: '9500115483'
+        tracking_number: '9500119500115483499'
     };
     request(server)
         .post("/veri")
@@ -21,11 +21,11 @@ it('Test 1: invalid tracking number when length < 20', function(done)
         .end(done);
 });
 
-it('Test 2: invalid tracking number when length > 25', function(done)
+it('Test 2: invalid tracking number when length = 26 > 25 (Boundary)', function(done)
 {
     let data = {
         usps_username: '328NOCOM1209',
-        tracking_number: '95001195001154834991494867035483499149486703'
+        tracking_number: '95001195001154834991494866'
     };
     request(server)
         .post("/veri")
@@ -97,26 +97,7 @@ it('Test 5: invalid tracking number when it has special characters', function(do
         .end(done);
 });
 
-it('Test 6: invalid tracking number when it does not have numbers', function(done)
-{
-    let data = {
-        usps_username: '328NOCOM1209',
-        tracking_number: '!!!!!!!!!!!!!!!!!!!!!!!'
-    };
-    request(server)
-        .post("/veri")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function (res)
-        {
-            assert.equal(res.body.status, 201);
-        })
-        .end(done);
-});
-
-it('Test 7: invalid tracking number when it only has lowercase letters', function(done)
+it('Test 6: invalid tracking number when it only has lowercase letters', function(done)
 {
     let data = {
         usps_username: '328NOCOM1209',
@@ -135,7 +116,7 @@ it('Test 7: invalid tracking number when it only has lowercase letters', functio
         .end(done);
 });
 
-it('Test 8: invalid tracking number when it only has uppercase letters', function(done)
+it('Test 7: invalid tracking number when it only has uppercase letters', function(done)
 {
     let data = {
         usps_username: '328NOCOM1209',
@@ -154,11 +135,68 @@ it('Test 8: invalid tracking number when it only has uppercase letters', functio
         .end(done);
 });
 
-it('Test 9: invalid tracking number when it only has special characters', function(done)
+it('Test 8: invalid tracking number when it only has special characters', function(done)
 {
     let data = {
         usps_username: '328NOCOM1209',
-        tracking_number: '!!!!~@@##$%%%%^&&&*****'
+        tracking_number: '?=.*?[#?!@$%^&*-?=.*?[#?'
+    };
+    request(server)
+        .post("/veri")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function (res)
+        {
+            assert.equal(res.body.status, 201);
+        })
+        .end(done);
+});
+
+// it('Test 9: invalid tracking number when it does not have numbers', function(done)
+// {
+//     let data = {
+//         usps_username: '328NOCOM1209',
+//         tracking_number: '?=.*GGGGGGGGGGGGGGGGGG'
+//     };
+//     request(server)
+//         .post("/veri")
+//         .send(data)
+//         .set('Accept', 'application/json')
+//         .expect('Content-Type', /json/)
+//         .expect(200)
+//         .expect(function (res)
+//         {
+//             assert.equal(res.body.status, 201);
+//         })
+//         .end(done);
+// });
+
+it('Test 9: invalid tracking number when there is no input', function(done)
+{
+    let data = {
+        usps_username: '328NOCOM1209',
+        tracking_number: ''
+    };
+    request(server)
+        .post("/veri")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function (res)
+        {
+            assert.equal(res.body.status, 201);
+        })
+        .end(done);
+});
+
+it('Test 10: invalid tracking number when input in null', function(done)
+{
+    let data = {
+        usps_username: '328NOCOM1209',
+        tracking_number: ' '
     };
     request(server)
         .post("/veri")
