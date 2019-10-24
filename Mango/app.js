88cbdb2e3f3ea7089f6f6a89bec588fdebed6c77
Dayu Liu <36178461+LDY681@users.noreply.github.com>
2019-10-25 07:17:26 +0800

    123
    123

--- Mango/app.js
+++ Mango/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
--- Mango/routes/index.js
+++ Mango/routes/index.js
@@ -5,7 +5,6 @@ var tracking = require('../packageInfo/TrackApi.js');
 var tool = require('../packageInfo/test.js');
 var path = require('path');
 
-
 var usps_username = '328NOCOM1209';		// DO NOT CHANGE THIS
 var tracking_number = '9500115483499149486703';
 
@@ -33,12 +32,19 @@ router.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, '../temp_front_files/index.html'))
 });
 
+router.post('/track', function(req, res){
+    console.log("json received: \n");
+    console.log(req.body);
+    //使用tracking_Num，通过tracking api求出tracking Info，然后将trackingInfo填进res.json里
+    res.json({ "usps_username": "liudayu","tracking_number": "test123"});
+});
+
 router.post('/', function(req, res, next){
-	var usps_username = req.body.usps_username;
-	var tracking_number = req.body.tracking_number;
-	console.log('usps_username: ', usps_username);
-	console.log('tracking_number: ', tracking_number);
-	console.log('body: ', req.body);
+    	var usps_username = req.body.usps_username;
+        var tracking_number = req.body.tracking_number;
+        console.log('usps_username: ', usps_username);
+        console.log('tracking_number: ', tracking_number);
+        console.log('body: ', req.body);
 });
 
 module.exports = router;
