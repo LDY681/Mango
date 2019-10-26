cd294d98bdb76518135f270000727a2e65095713
Xingyu Wang <brandon.xyw@gmail.com>
2019-10-26 05:13:36 -0400

    clean

--- Mango/models/tag.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const tagSchema = mongoose.Schema({
-  title: {type: String, required: true,},
-  username: {type: String},
-})
-
-module.exports = mongoose.model("tag", tagSchema, 'tag');
