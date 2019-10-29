95ce06ee1790701f14ca41048074302404d7e8f5
Xingyu Wang <brandon.xyw@gmail.com>
2019-10-28 21:09:25 -0400

    track post (map)

--- Mango/routes/map.js
+++ Mango/routes/map.js
@@ -9,17 +9,17 @@ router.get('/', (req, res) => {
     //var result = readData.getName("wang1111@purdue.edu");
     //console.log("result: " + JSON.stringify(result));
     //console.log(readData.math());
-    var obj = new Info(5);
+    //var obj = new Info(5);
     //obj.math(obj);
     //obj.getName("wang1111@purdue.edu", obj);
     //obj.test();
     //console.log(obj.name);
     
-    async function printName(){
-        var name = await obj.getName("wang1111@purdue.edu");
-        console.log(name);
-    }
-    printName();
+    // async function printName(){
+    //     var name = await obj.getName("wang1111@purdue.edu");
+    //     console.log(name);
+    // }
+    // printName();
 
     //res.sendFile(path.join(__dirname, '../temp_front_files/sign_up.html'));
     res.sendFile(path.join(__dirname, '../map/html/map.html'));
