aaa293e76569514aa3379c14bc2bc0dfa2f913c4
Kason <miao29@purdue.edu>
2019-11-01 09:11:12 -0400

    Dynamical Info Page

--- Mango/routes/info.js
+++ Mango/routes/info.js
@@ -5,8 +5,7 @@ var tool = require('../packageInfo/test.js');
 var path = require('path');
 
 var usps_username = '328NOCOM1209';     // DO NOT CHANGE THIS
-var tracking_number = '9500115483499149486703';
-
+var tracking_number = '';
 
 
 var packageInfo;
@@ -15,23 +14,31 @@ function usps_callback(response) {
 	packageInfo = response;
 }
 
-
-
 /* GET users listing. */
 router.get('/',  async (req, res) => {
-	// var response = {
-	// 	"tracking number":req.query.tracking_num,
-	// };
+	var response = {
+		"tracking number":req.query.tracking_num,
+	};
+	tracking_number = req.query.tracking_num;
+	console.log("response");
+	console.log(response);
 
 	trackingAPI.trackUSPS(usps_username, tracking_number, usps_callback);
 
-	// sleep 1 second, wait untail package data is fetched 
+	// sleep 1 second, wait untail package data is fetched
 	await new Promise (resolve => {
 		setTimeout(resolve, 1000)
 	});
-
-	//console.log(packageInfo.TrackResponse.TrackInfo[0].$);
-	//res.end(JSON.stringify(packageInfo));
+	//printing tracking number
+	console.log();
+	console.log("printing tracking id from info.js");
+	console.log(packageInfo.TrackResponse.TrackInfo[0].$);
+	console.log();
+	console.log("printing packageinfo");
+	console.log(JSON.stringify(packageInfo));
+	console.log();
+	//printing tracking info and do not render info.html
+	// res.end(JSON.stringify(packageInfo));
 	res.sendFile(path.join(__dirname, '../temp_front_files/info.html'))
 });
 
