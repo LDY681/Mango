cba9df87994ba6cac0716e20789204615ca97746
WUu422 <wu1037@purdue.edu>
2019-10-31 22:32:18 -0400

    new test for finding records in database

---
+++ Mango/tests/test_db_finding.js
@@ -0,0 +1,34 @@
+var request = require("supertest");
+var server = require("../app.js");
+var assert = require("assert");
+var Package = require("../models/Package");
+
+describe('Finding records', function(){
+    var char;
+
+    beforeEach(function(done){
+        char = new Package({
+            tracking_number: '9500119500115483000'
+        });
+        char.save().then(function() {
+            assert(char.isNew === false);
+            done();
+        });
+    });
+
+    // it('find one record from the database', function(done) {
+    //     this.timeout(15000);
+    //     setTimeout(done, 13000);
+    //     Package.findOne({tracking_number: '9500119500115483000'}).then(function (result) {
+    //         assert(result.tracking_number==='9500119500115483000');
+    //         done();
+    //     });
+    // });
+
+    it('find one record by ID from the database', function(done) {
+        Package.findOne({_id:char._id}).then(function (result) {
+            assert(result._id.toString() === char._id.toString());
+            done();
+        });
+    });
+});
